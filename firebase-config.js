176f2c743eaa1d3e8dbbb1ec2ef64a57dd1fa902
godowner202 <125318362+godowner202@users.noreply.github.com>
2025-04-18 22:56:24 +0200

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAEzvb3cVNB9PdG6BNnm4mGfAsj2QWaEiQ",
+  authDomain: "refstorm-e787d.firebaseapp.com",
+  projectId: "refstorm-e787d",
+  storageBucket: "refstorm-e787d.firebasestorage.app",
+  messagingSenderId: "234472749361",
+  appId: "1:234472749361:web:0f00e2a3a5abbb07025810"
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
